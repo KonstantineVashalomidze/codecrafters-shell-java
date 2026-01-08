249097eb46b0b556c69348563e071ec4be5c260d
KonstantineVashalomidze <vashalomidzekonstantine@gmail.com>
2026-01-08 23:18:44 +0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String command = null;
+        String command;
         System.out.print("$ ");
         while ((command = scanner.nextLine()) != null) {
             if (command.startsWith("type")) {
@@ -12,7 +16,32 @@ public class Main {
                 if (Set.of("exit", "echo", "type").contains(typeArg)) {
                     System.out.println(typeArg + " is a shell builtin");
                 } else {
-                    System.out.println(typeArg + ": not found");
+                    String[] pathsToCheck = System.getenv("PATH")
+                            .split(File.pathSeparator);
+                    boolean foundExecutable = false;
+                    for (String p : pathsToCheck) {
+                        Path pathToCheck = Path.of(p);
+                        if (Files.exists(pathToCheck)) {
+                            try (Stream<Path> files = Files.list(pathToCheck)) {
+                                foundExecutable = files.anyMatch(path -> {
+                                    if (path.getFileName().toString().equals(typeArg)
+                                            && Files.isExecutable(path)) {
+                                        System.out.println(typeArg + " is " + pathToCheck.resolve(typeArg));
+                                        return true;
+                                    } else {
+                                        return false;
+                                    }
+                                });
+                            }
+                        }
+                        if (foundExecutable)
+                            break;
+
+                    }
+                    if (!foundExecutable) {
+                        System.out.println(typeArg + ": not found");
+                    }
+
                 }
             } else if (command.equals("exit")) break;
             else if (command.startsWith("echo")) {
