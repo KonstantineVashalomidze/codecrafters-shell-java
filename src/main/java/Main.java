3661c87c334ba09f14bee0dfa4ba7a3677b1e9e3
KonstantineVashalomidze <vashalomidzekonstantine@gmail.com>
2026-01-08 22:42:31 +0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -6,7 +7,14 @@ public class Main {
         String command = null;
         System.out.print("$ ");
         while ((command = scanner.nextLine()) != null) {
-            if (command.equals("exit")) break;
+            if (command.startsWith("type")) {
+                String typeArg = command.split(" ", 2)[1];
+                if (Set.of("exit", "echo", "type").contains(typeArg)) {
+                    System.out.println(typeArg + " is a shell builtin");
+                } else {
+                    System.out.println(typeArg + ": not found");
+                }
+            } else if (command.equals("exit")) break;
             else if (command.startsWith("echo")) {
                 String echoArg = command.split(" ", 2)[1];
                 System.out.println(echoArg);
